fcc072ddbfe9b2fd89809daadb20001734f55af9
AryanRastogi7767 <aryanrastogi97@gmail.com>
2025-01-28 19:01:17 +0530

    added directory structure

---
+++ backend/data_processing/preprocessing.py
@@ -0,0 +1,5 @@
+#TODO: 1. create a general purpose chunking code using unstructured library
+#TODO 2. create  it in such a way that anyone can use it directly by
+#plugging in the folder of documents and create chunks
+# also figure out if we can make a general pipeline to create embeddings 
+# from the chunks obtained from the folder.

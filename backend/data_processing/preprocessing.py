b57a44104c8b0a7b026c36d34ea5d28ea718a347
AryanRastogi7767 <aryanrastogi97@gmail.com>
2025-01-30 09:49:01 +0530

    preprocessing in jupyter notebook

--- backend/data_processing/preprocessing.py
+++ backend/data_processing/preprocessing.py
@@ -1,5 +1,13 @@
-#TODO: 1. create a general purpose chunking code using unstructured library
-#TODO 2. create  it in such a way that anyone can use it directly by
-#plugging in the folder of documents and create chunks
-# also figure out if we can make a general pipeline to create embeddings 
+# TODO: 1. create a general purpose chunking code using unstructured library
+# TODO 2. create  it in such a way that anyone can use it directly by
+# plugging in the folder of documents and create chunks
+# also figure out if we can make a general pipeline to create embeddings
 # from the chunks obtained from the folder.
+
+import os
+
+from unstructured.partition.pdf import partition_pdf
+
+PDF_FOLDER = "../../policy_documents/"
+PDF_LIST = os.listdir(PDF_FOLDER)
+print(PDF_LIST)
